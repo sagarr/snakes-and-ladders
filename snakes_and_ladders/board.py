63c7fd005a6ba4509ac0682613d59a51f763b0ea
sagarr <sagar.rohankar@gmail.com>
2021-03-13 17:12:06 +0530

    Story 2: Add a snake on the board - Add snake board as wrapper to basic board - Snake moves player from snakes start position to end position

--- snakes_and_ladders/board.py
+++ snakes_and_ladders/board.py
@@ -9,3 +9,28 @@ class Board:
             self.player_position += dice_thrown
 
         return self.player_position
+
+
+class SnakeBoard:
+
+    def __init__(self, snake_positions):
+        self.board = Board()
+        self.snake_positions = {}
+        for start_pos, end_pos in snake_positions:
+            if start_pos in self.snake_positions:
+                raise ValueError(f"Snake already placed at {start_pos}!")
+            self.snake_positions[start_pos] = end_pos
+
+    def get_snake_positions(self):
+        for start_pos, end_pos in self.snake_positions.items():
+            yield start_pos, end_pos
+
+    def move(self, dice_thrown):
+        player_position = self.board.move(dice_thrown)
+
+        if player_position in self.snake_positions:
+            # player moved to snake start position, descend to snake end position
+            end_position = self.snake_positions[player_position]
+            player_position = self.board.move(end_position - player_position)
+
+        return player_position
---
+++ snakes_and_ladders/tests/test_snake_board.py
@@ -0,0 +1,27 @@
+import pytest
+
+from snakes_and_ladders.board import SnakeBoard
+
+
+def test_snake_start_position_greater_than_end_position_on_board():
+    snake_board = SnakeBoard(snake_positions=[(14, 7)])
+
+    for snake_start_pos, snake_end_pos in snake_board.get_snake_positions():
+        assert snake_start_pos > snake_end_pos
+
+
+def test_duplicate_snake_start_position_not_allowed_on_board():
+    with pytest.raises(ValueError):
+        SnakeBoard(snake_positions=[(14, 7), (14, 1)])
+
+
+# TODO parametrized tests for various start and end positions
+def test_snake_bite_moves_player_from_snake_start_position_to_end_position():
+    snake_board = SnakeBoard(snake_positions=[(14, 7)])
+
+    snake_board.move(6)  # 7
+    snake_board.move(6)  # 13
+
+    player_position = snake_board.move(1)  # 14
+
+    assert player_position == 7
